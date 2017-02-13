ca9b7908fbdab5122b0f14fab635f6f375c75608
Vasileios Zois <vzois@usc.edu>
2017-02-12 21:25:31 -0800

    sse benchmark

--- cpu_sgemm/score.h
+++ cpu_sgemm/score.h
@@ -83,7 +83,7 @@ void dgemm_reg_blocking(const double *A, const double *B, double *&C, uint64_t N
 	}
 }
 
-void dgemm_cache(const double *A, const double *B, double *&C, uint64_t N){
+void dgemm_cache2(const double *A, const double *B, double *&C, uint64_t N){
 	for(uint64_t i = 0; i < N ; i++){
 		for(uint64_t k = 0; k < N ;k+=4){
 			register uint64_t iA = i * N + k;
@@ -110,6 +110,17 @@ void dgemm_cache(const double *A, const double *B, double *&C, uint64_t N){
 	}
 }
 
+void dgemm_cache(const double *A, const double *B, double *&C, uint64_t N){
+	for(uint64_t i = 0; i < N ;i++){
+		for(uint64_t k = 0; k < N ;k++){
+			register double rA = A[i * N + k];
+			for(uint64_t j = 0; j < N ;j++){
+				C[i * N + j] += rA * B[ k * N + j ];
+			}
+		}
+	}
+}
+
 void dgemm_block(const double *A, const double *B, double *&C, uint64_t N){
 	uint64_t Bz = 32;
 	for(uint64_t i = 0; i < N; i+=Bz){
@@ -131,15 +142,10 @@ void dgemm_block(const double *A, const double *B, double *&C, uint64_t N){
 	}
 }
 
-void dgemm_cache2(const double *A, const double *B, double *&C, uint64_t N){
+void dgemm_cache_sse(const double *A, const double *B, double *&C, uint64_t N){
 	for(uint64_t i = 0; i < N ; i++){
 		for(uint64_t k = 0; k < N ;k+=4){
 			register uint64_t iA = i * N + k;
-			//register double rA = A[ iA ];
-			//register double rA1 = A[ iA + 1 ];
-			//register double rA2 = A[ iA + 2 ];
-			//register double rA3 = A[ iA + 3 ];
-
 			__m128d vA1 = _mm_load_pd1(&A[iA]);//rA
 			__m128d vA2 = _mm_load_pd1(&A[iA+1]);//rA1
 			__m128d vA3 = _mm_load_pd1(&A[iA+2]);//rA2
@@ -177,12 +183,6 @@ void dgemm_cache2(const double *A, const double *B, double *&C, uint64_t N){
 
 				_mm_store_pd(&C[iC],vC1);
 				_mm_store_pd(&C[iC+2],vC2);
-
-				//_mm_mul_pd(vA1,vB1);
-				//C[iC] += rA * B[ iB ] + rA1 * B[ iiB ] + rA2 * B[ iiiB ] + rA3 * B[ iiiiB ];
-				//C[iC + 1] += rA * B[ iB + 1 ] + rA1 * B[ iiB + 1 ] + rA2 * B[ iiiB + 1 ] + rA3 * B[ iiiiB + 1 ];
-				//C[iC + 2] += rA * B[ iB + 2 ] + rA1 * B[ iiB + 2 ] + rA2 * B[ iiiB + 2 ] + rA3 * B[ iiiiB + 2 ];
-				//C[iC + 3] += rA * B[ iB + 3 ] + rA1 * B[ iiB + 3 ] + rA2 * B[ iiiB + 3 ] + rA3 * B[ iiiiB + 3 ];
 			}
 		}
 	}
@@ -212,9 +212,15 @@ void dgemm_score_main(double *A, double *B, double *&C, double *&D, uint64_t N){
 
 	//4
 	t.start();
-	dgemm_cache2(A,B,D,N);
+	dgemm_cache(A,B,D,N);
 	double dgemm_cache = t.lap("dgemm_cache elapsed time in secs");
 	cmpResults(A,B,C,D,N,"dgemm_base","dgemm_cache");
+	zeros(D,N);
+
+	t.start();
+	dgemm_cache_sse(A,B,D,N);
+	double dgemm_cache_sse = t.lap("dgemm_cache_sse elapsed time in secs");
+	cmpResults(A,B,C,D,N,"dgemm_base","dgemm_cache_sse");
 	zeros(D,N);
 
 	//5
@@ -235,6 +241,7 @@ void dgemm_score_main(double *A, double *B, double *&C, double *&D, uint64_t N){
 	std::cout<< "Elapsed time for dgemm_regC: " << dgemm_regC << " seconds\n";
 	std::cout<< "Elapsed time for dgemm_reg_blocking: " << dgemm_reg_blocking << " seconds\n";
 	std::cout<< "Elapsed time for dgemm_cache: " << dgemm_cache << " seconds\n";
+	std::cout<< "Elapsed time for dgemm_cache_sse: " << dgemm_cache_sse << " seconds\n";
 	std::cout<< "Elapsed time for dgemm_block: " << dgemm_block << " seconds\n";
 	std::cout<< "Elapsed time for dgemm_cblas: " << dgemm_cblas << " seconds\n";
 
@@ -242,10 +249,9 @@ void dgemm_score_main(double *A, double *B, double *&C, double *&D, uint64_t N){
 	std::cout << "GFLOPS for dgemm_regC: " << ((double)(GFLOPS/dgemm_regC))/1000000000 << "\n";
 	std::cout << "GFLOPS for dgemm_reg_blocking: " << ((double)(GFLOPS/dgemm_reg_blocking))/1000000000 << "\n";
 	std::cout << "GFLOPS for dgemm_cache: " << ((double)(GFLOPS/dgemm_cache))/1000000000 << "\n";
+	std::cout << "GFLOPS for dgemm_cache_sse: " << ((double)(GFLOPS/dgemm_cache_sse))/1000000000 << "\n";
 	std::cout << "GFLOPS for dgemm_block: " << ((double)(GFLOPS/dgemm_block))/1000000000 << "\n";
 	std::cout << "GFLOPS for dgemm_cblas: " << ((double)(GFLOPS/dgemm_cblas))/1000000000 << "\n";
-
-
 }
 
 #endif
